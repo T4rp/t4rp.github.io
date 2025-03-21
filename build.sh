aad7a8f9f1a007ae2ce7df9976a430f127c6dca5
T4rp <why9ten@gmail.com>
2025-03-21 15:52:24 -0400

    fix: dont use jekyll to build some directories

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-pandoc -s ./layouts/index.html --template ./layouts/template.html -o ./index.html
-pandoc -s ./layouts/markdown-tester.md --template ./layouts/template.html -o ./markdown-tester.html
-pandoc -s ./layouts/blogs.md --template ./layouts/template.html -o ./blogs.html
+pandoc -s ./_layouts/index.html --template ./_layouts/template.html -o ./index.html
+pandoc -s ./_layouts/markdown-tester.md --template ./_layouts/template.html -o ./markdown-tester.html
+pandoc -s ./_layouts/blogs.md --template ./_layouts/template.html -o ./blogs.html
 
-pandoc -s ./blogs/2025-3-21-first-post.md --template ./layouts/blog-template.html -o ./2025-3-21-first-post.html
+pandoc -s ./_blogs/2025-3-21-first-post.md --template ./_layouts/blog-template.html -o ./2025-3-21-first-post.html
