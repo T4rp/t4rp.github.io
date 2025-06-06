9268b344e34b96d53d2155d911949260cf149f0f
T4rp <why9ten@gmail.com>
2025-06-06 10:59:37 -0400

    feat: Make the shittiest CMS

--- build.py
+++ build.py
@@ -1,3 +1,4 @@
+from typing import Counter
 import jinja2
 import markdown
 import os
@@ -12,27 +13,29 @@ env = jinja2.Environment(loader=jinja2.FileSystemLoader("./templates"))
 
 template = env.get_template("template.html")
 
-def render_markdown(item):
+def render_content(item):
   item_path = path.join(CONTENT_DIR, item)
   if not path.isfile(item_path):
     return
 
+  split_path = path.splitext(item)
+
   with open(item_path) as f:
     content = f.read()
-    content_html = markdown.markdown(content)
-    rendered_content = template.render(content=content_html)
 
-  dest_path = path.join(DEST_DIR, path.basename(item) + ".html")
+    if split_path[1] == ".md":
+      content = markdown.markdown(content)
+
+    rendered_content = template.render(content=content)
+
+  dest_path = path.join(DEST_DIR, split_path[0] + ".html")
   with open(dest_path, "w") as f:
     f.write(rendered_content)
 
 
-
-for item in os.listdir(CONTENT_DIR):
-  render_markdown(item)
-
-
-render_markdown("markdown-tester.md")
+render_content("markdown-tester.md")
+render_content("blogs.md")
+render_content("index.html")
 
 shutil.copytree(path.join(".", "styles"), DEST_DIR, dirs_exist_ok=True)
 shutil.copytree(path.join(".", "scripts"), DEST_DIR, dirs_exist_ok=True)
